166681bebea000c1785963d2243b68e6f817868d
pedroquack <pedrovolpattocosta@gmail.com>
2024-07-20 18:57:02 -0300

    Modify font-size in mobile, and add a responsive grid on the task forms

--- resources/views/livewire/task-create.blade.php
+++ resources/views/livewire/task-create.blade.php
@@ -7,13 +7,12 @@
             @error('nome') <span class="error-message">{{ $message }}</span> @enderror
         </div>
         <div class="grid grid-cols-2 gap-3">
-            <div class="form-group">
+            <div class="form-group col-span-2 md:col-span-1">
                 <label for="data">Data de expiração <small>(opcional)</small></label>
                 <input type="datetime-local" wire:model="data" id="data" class="@error('data') error @enderror">
                 @error('data') <span class="error-message">{{ $message }}</span> @enderror
             </div>
-
-            <div class="form-group">
+            <div class="form-group col-span-2 md:col-span-1">
                 <label for="prioridade">Prioridade</label>
                 <select wire:model="prioridade" id="prioridade" class="@error('prioridade') error @enderror">
                     <option value="1">Alta</option>
--- resources/views/livewire/task-edit.blade.php
+++ resources/views/livewire/task-edit.blade.php
@@ -7,13 +7,13 @@
             @error('nome') <span class="error-message">{{ $message }}</span> @enderror
         </div>
         <div class="grid grid-cols-2 gap-3">
-            <div class="form-group">
+            <div class="form-group col-span-2 md:col-span-1">
                 <label for="data">Data de expiração <small>(opcional)</small></label>
                 <input type="datetime-local" wire:model="data" id="data" class="@error('data') error @enderror" value="{{ $task->date }}">
                 @error('data') <span class="error-message">{{ $message }}</span> @enderror
             </div>
 
-            <div class="form-group">
+            <div class="form-group col-span-2 md:col-span-1">
                 <label for="prioridade">Prioridade</label>
                 <select wire:model="prioridade" id="prioridade" class="@error('prioridade') error @enderror">
                     <option value="1" @if($task->priority === '1') selected @endif>Alta</option>
